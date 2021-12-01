4ce14c5c81a950069596cc598183197d648793c5
racray <rayedkhan007@gmail.com>
2021-12-01 21:12:36 +0530

    id to _id

--- GetMovie.js
+++ GetMovie.js
@@ -1,4 +1,5 @@
 import { client } from "./index.js";
+import { ObjectId } from "mongodb";
 
 export async function GetMovie(filter) {
     return await client.db("b28wd").collection("movies").find(filter).toArray();
@@ -7,11 +8,11 @@ export async function PostMovieById(data) {
     return await client.db("b28wd").collection("movies").insertMany(data);
 }
 export async function GetMovieById(id) {
-    return await client.db("b28wd").collection("movies").findOne({ id: id });
+    return await client.db("b28wd").collection("movies").findOne({ _id: ObjectId(id) });
 }
 export async function DeleteMovieById(id) {
-    return await client.db("b28wd").collection("movies").deleteOne({ id: id });
+    return await client.db("b28wd").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function EditMovieById(id, data) {
-    return await client.db("b28wd").collection("movies").updateOne({ id: id }, { $set: data });
+    return await client.db("b28wd").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
