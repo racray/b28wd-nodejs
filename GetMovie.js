2ca8fc0ac34ce74b3f62d7b630d71ac06f4125a9
racray <rayedkhan007@gmail.com>
2021-12-02 09:43:00 +0530

    done

--- GetMovie.js
+++ GetMovie.js
@@ -5,7 +5,7 @@ export async function GetMovie(filter) {
     return await client.db("b28wd").collection("movies").find(filter).toArray();
 }
 export async function PostMovieById(data) {
-    return await client.db("b28wd").collection("movies").insertMany(data);
+    return await client.db("b28wd").collection("movies").insertOne(data);
 }
 export async function GetMovieById(id) {
     return await client.db("b28wd").collection("movies").findOne({ _id: ObjectId(id) });
